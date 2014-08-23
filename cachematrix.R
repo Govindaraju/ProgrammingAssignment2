8cb97c467b440a9d77fe9ccef01c67ad44e489a5
Govindaraju <govind.kalyana@gmail.com>
2014-08-23 21:01:17 +0100

    added method comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Please follow the below steps to test the functions defined below
+## mat <- matrix(1:4,2,2)
+## cacheasblemat <- makeCacheMatrix(mat)
+## inv1 <- cacheSolve(cachablemat)
+## in the above step the inverse will be calculated and returned.
+## inv2 <- cacheSolve(cachablemat)
+## in the above step the inverse wont be calculated rather will be returned from the cache
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix and returns an object which has methods to get and set the matrix
+## attribute and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
@@ -19,7 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - returns an inverse of a matrix present within x which is of type makeCacheMatrix
+## cachesolve first checks if the inverse exist for a given matrix, if present it returns it from
+## it from the cache if not it calculates inverse, sets it to x and then returns the calculated
+## inverse of the matrix
 
 cacheSolve <- function(x = makeCacheMatrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
